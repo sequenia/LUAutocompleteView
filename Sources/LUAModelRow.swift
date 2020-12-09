8ce1a63ce2eaedfc25037c0caad3930a8d267130
Ivan Michaylovsky <ivan.michaylovsky@sequenia.com>
2020-12-09 12:34:00 +0700

    add model row

---
+++ Sources/LUAModelRow.swift
@@ -0,0 +1,17 @@
+//
+//  LUAModelRow.swift
+//  LUAutocompleteView
+//
+//  Created by Ivan Mikhailovskii on 09.12.2020.
+//
+
+import Foundation
+
+open class LUAModelRow {
+    
+    public var id: String
+    
+    public required init(id: String) {
+        self.id = id
+    }
+}
